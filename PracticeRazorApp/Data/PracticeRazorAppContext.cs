90ab98046bc8032feb1e3fa225b61668f8604edd
Jessica Handley <jhandley1000@gmail.com>
2018-07-02 23:51:44 +1000

    Add project files.

---
+++ PracticeRazorApp/Data/PracticeRazorAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PracticeRazorApp.Models
+{
+    public class PracticeRazorAppContext : DbContext
+    {
+        public PracticeRazorAppContext (DbContextOptions<PracticeRazorAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PracticeRazorApp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ PracticeRazorApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PracticeRazorApp.Models;
+
+namespace PracticeRazorApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+			using (var scope = host.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+
+				try
+				{
+					//eventually i guess this will include populating db
+					var context = services.GetRequiredService<PracticeRazorAppContext>();
+					context.Database.Migrate();
+					SeedData.Initialize(services);
+				}
+				catch (Exception ex)
+				{
+					var logger = services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "An error occurred seeding the DB.");
+				}
+			}
+
+			host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
